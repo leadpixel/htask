ce775edf5e3dc493c70cf0e72366198cd1518c57
Jack Palfrey <jack.palf@gmail.com>
2018-02-05 21:59:36 +0000

    adding capability to add tasks

--- packages/htask/lib/Lib.hs
+++ packages/htask/lib/Lib.hs
@@ -1,33 +1,45 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ConstraintKinds #-}
 
-import qualified Data.UUID as UUID
-import System.Random
-import Control.Monad.IO.Class
-import qualified Control.Monad.Reader as Reader
-import qualified Control.Monad.State as State
-import Data.Semigroup
+module Lib
+  ( TaskEventType (..)
+  , buildEventTree
+  , displayTree
+  ) where
+
 import Conduit
-import qualified Data.Text as T
+import Control.Monad.IO.Class
+import Data.Semigroup
+import System.Random
+import Data.Foldable
+import qualified Control.Monad.Reader as Reader
+import Control.Monad
+import qualified Control.Monad.State as State
 import qualified Data.ByteString as BS
 import qualified Data.Map as Map
-import Data.Time
+import qualified Data.Text as Text
+import qualified Data.Time as Time
+import qualified Data.Time.Clock.System as Time
+import qualified Data.Tree as Tree
+import qualified Data.UUID as UUID
+import qualified Data.UUID.V4 as UUID
 
 
 type TaskUUID = UUID.UUID
-type Timestamp = UTCTime
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+type Timestamp = Time.UTCTime
+type TaskTree = Tree.Tree Task
 
 
-data TaskEvent
-  = TaskAdd
+data TaskEventType
+  = TaskAdd Text.Text
   | TaskComplete
   | TaskAbandon
   | TaskAssign
+  deriving (Show)
 
+
+data TaskEvent = TaskEvent TaskUUID Timestamp TaskEventType
+  deriving (Show)
 
 
 data TaskStatus
@@ -40,8 +52,93 @@ data TaskStatus
 
 
 data Task = Task
-  { taskUuid :: TaskUUID
-  , taskDescription :: T.Text
-  , taskCreatedAt :: Timestamp
-    , taskStatus :: TaskStatus
+  { uuid :: TaskUUID
+  , description :: Text.Text
+  , createdAt :: Timestamp
+  , status :: TaskStatus
+  } deriving (Show)
+
+
+displayTask :: Task -> String
+displayTask t = if UUID.null (uuid t)
+                   then "root"
+                   else show t
+
+
+displayTree :: TaskTree -> String
+displayTree t = show (displayTask <$> t)
+
+
+class CanTime m where
+  now :: m Timestamp
+
+instance CanTime IO where
+  now = Time.systemToUTCTime <$> Time.getSystemTime
+
+
+class CanUuid m where
+  uuidGen :: m TaskUUID
+
+instance CanUuid IO where
+  uuidGen = UUID.nextRandom
+
+
+
+
+type CanCreateTask m = (Monad m, CanTime m, CanUuid m)
+
+
+
+readTaskFile :: FilePath -> [TaskEvent]
+readTaskFile = undefined
+
+
+buildEventTree :: (Foldable f, Traversable f, CanCreateTask m) => f TaskEventType -> m TaskTree
+buildEventTree tts = mapM k tts >>= parseEvents
+  where
+    k :: (CanCreateTask m) => TaskEventType -> m TaskEvent
+    k t = TaskEvent <$> uuidGen <*> now <*> pure t
+
+
+parseEvents :: (Foldable f, CanCreateTask m) => f TaskEvent -> m TaskTree
+parseEvents ts = foldM k emptyTree ts
+
+  where
+    k :: (CanCreateTask m) => TaskTree -> TaskEvent -> m TaskTree
+    k tr ev@(TaskEvent evId ts evTy)
+      = case evTy of
+          TaskAdd t -> addTask tr <$> createTask t
+
+
+
+
+createTask :: (CanCreateTask m) => Text.Text -> m Task
+createTask t = mkTask <$> uuidGen <*> now
+  where
+    mkTask :: TaskUUID -> Timestamp -> Task
+    mkTask u s = Task u t s Pending
+
+
+addTask :: TaskTree -> Task -> TaskTree
+addTask r@(Tree.Node l ps) t = Tree.Node l (tn : ps)
+  where
+    tn :: TaskTree
+    tn = Tree.Node t []
+
+
+findParent :: TaskTree -> Task -> Maybe Task
+findParent r t = Nothing
+
+
+rootTask :: Task
+rootTask = Task
+  { uuid = UUID.nil
+  , description = "root"
+  , createdAt = Time.UTCTime (Time.ModifiedJulianDay 0) (Time.secondsToDiffTime 0)
+  , status = Pending
   }
+
+
+emptyTree :: TaskTree
+emptyTree = Tree.Node rootTask []
+
--- packages/htask/src/Main.hs
+++ packages/htask/src/Main.hs
@@ -1,6 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Lib
 
+import Conduit
+import Control.Monad.IO.Class
+import Data.Semigroup
+import System.Random
+import Data.Foldable
+import qualified Control.Monad.Reader as Reader
+import Control.Monad
+import qualified Control.Monad.State as State
+import qualified Data.ByteString as BS
+import qualified Data.Map as Map
+import qualified Data.Text as Text
+import qualified Data.Time as Time
+import qualified Data.Time.Clock.System as Time
+import qualified Data.Tree as Tree
+import qualified Data.UUID as UUID
+import qualified Data.UUID.V4 as UUID
+
+
 main :: IO ()
-main = someFunc
+main = do
+  buildEventTree [] >>= print . displayTree
+  buildEventTree [ TaskAdd "test" ] >>= print . Tree.levels
+  buildEventTree [ TaskAdd "test", TaskAdd "test2" ] >>= print . Tree.levels
+  buildEventTree [ TaskAdd "test", TaskAdd "test2" ] >>= print . Tree.levels
