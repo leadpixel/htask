55cc91b24237e183ec460eaf196b6a4501f6b886
Jack Palfrey <jack.palf@gmail.com>
2018-02-05 11:06:37 +0000

    adding some potential dependencies

--- packages/htask/lib/Lib.hs
+++ packages/htask/lib/Lib.hs
@@ -2,5 +2,46 @@ module Lib
     ( someFunc
     ) where
 
+import qualified Data.UUID as UUID
+import System.Random
+import Control.Monad.IO.Class
+import qualified Control.Monad.Reader as Reader
+import qualified Control.Monad.State as State
+import Data.Semigroup
+import Conduit
+import qualified Data.Text as T
+import qualified Data.ByteString as BS
+import qualified Data.Map as Map
+import Data.Time
+
+
+type TaskUUID = UUID.UUID
+type Timestamp = UTCTime
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+data TaskEvent
+  = TaskAdd
+  | TaskComplete
+  | TaskAbandon
+  | TaskAssign
+
+
+
+data TaskStatus
+  = Pending
+  | Started
+  | Blocked
+  | Complete
+  | Aborted
+  deriving (Show, Eq)
+
+
+data Task = Task
+  { taskUuid :: TaskUUID
+  , taskDescription :: T.Text
+  , taskCreatedAt :: Timestamp
+    , taskStatus :: TaskStatus
+  }
