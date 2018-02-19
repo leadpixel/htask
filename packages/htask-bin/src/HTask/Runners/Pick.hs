1d7f832d5db6253629fe866b2045ab35ec9e5fe5
Jack Palfrey <jack.palf@gmail.com>
2018-02-19 16:06:08 +0000

    using random select for pick

--- packages/htask-bin/src/HTask/Runners/Pick.hs
+++ packages/htask-bin/src/HTask/Runners/Pick.hs
@@ -4,11 +4,20 @@ module HTask.Runners.Pick
   ( runPick
   ) where
 
+import System.Random
 import Control.Monad.Reader
 import HTask.TaskApplication
 import Data.Semigroup ((<>))
 import qualified HTask as H
 import qualified Data.Text as Text
+
+
+class (Monad m) => CanRandom m where
+  getRandomR :: (Random a) => (a, a) -> m a
+
+
+instance CanRandom IO where
+  getRandomR = randomRIO
 
 
 hasStatus :: H.TaskStatus -> H.Task -> Bool
@@ -19,10 +28,11 @@ runPick :: TaskConfig ()
 runPick = do
   ts <- runTask H.listTasks
   let ps = filter (hasStatus H.Pending) ts
+  k <- lift $ randomSelectOne ps
   maybe
-    (lift (putStrLn "no task to pick"))
+    (lift $ putStrLn "no task to pick")
     (startTask)
-    (randomSelectOne ps)
+    (k)
   pure ()
 
   where
@@ -31,6 +41,8 @@ runPick = do
       lift $ putStrLn (Text.unpack $ "picking task: " <> H.description t)
 
 
-randomSelectOne :: [a] -> Maybe a
-randomSelectOne [] = Nothing
-randomSelectOne (x:_) = Just x
+randomSelectOne :: (CanRandom m) => [a] -> m (Maybe a)
+randomSelectOne [] = pure Nothing
+randomSelectOne xs = do
+  n <- getRandomR (0, length xs)
+  pure $ Just $ xs !! n
