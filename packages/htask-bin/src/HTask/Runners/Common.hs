c82b158ef68049d5082cfddc9c09d248f77bd05e
Jack Palfrey <jack.palf@gmail.com>
2018-03-19 08:42:43 +0000

    split runWith* and find* Task

--- packages/htask-bin/src/HTask/Runners/Common.hs
+++ packages/htask-bin/src/HTask/Runners/Common.hs
@@ -2,6 +2,7 @@
 
 module HTask.Runners.Common
   ( runWithMatch
+  , findMatch
   ) where
 
 import qualified Data.Text as Text
@@ -13,25 +14,44 @@ import Data.Semigroup ((<>))
 import Data.Tagged
 
 
-justOne :: [a] -> Maybe a
-justOne [x] = Just x
-justOne _ = Nothing
-
-
-findMatchingUUIDs :: Text.Text -> [H.Task] -> [Text.Text]
-findMatchingUUIDs ref ts = filter (ref `Text.isPrefixOf`) (fmap taskRefToText ts)
+headSafe :: [a] -> Maybe a
+headSafe [x] = Just x
+headSafe _ = Nothing
 
 
 taskRefToText :: H.Task -> Text.Text
 taskRefToText = UUID.toText . untag . H.taskRef
 
 
-runWithMatch :: (Show a) => (H.TaskRef -> TaskApplication a) -> Text.Text -> TaskConfig Document
+findMatch :: Text.Text -> TaskConfig (Maybe H.Task)
+findMatch ref
+  = (headSafe . filterMatchesUUID ref) <$> runTask H.listTasks
+
+ where
+   filterMatchesUUID :: Text.Text -> [H.Task] -> [H.Task]
+   filterMatchesUUID ref
+     = filter (Text.isPrefixOf ref . taskRefToText)
+
+
+runWithMatch
+  :: (Show a)
+  => (H.TaskRef -> TaskApplication a)
+  -> Text.Text
+  -> TaskConfig Document
 runWithMatch f ref
-  = do
-    ts <- runTask H.listTasks
-    output <- maybe
-      (pure $ "did not find unique match for: " <> ref)
-      (\v -> (Text.pack . show) <$> runTask (f $ Tagged v))
-      (justOne (findMatchingUUIDs ref ts) >>= UUID.fromText)
-    pure $ Document [ line output ]
+  = findMatch ref
+  >>= maybe
+      (pure $ formatNoMatchError ref)
+      ((fmap . fmap) coerceToSuccess (j f))
+
+  where
+    formatNoMatchError :: Text.Text -> Document
+    formatNoMatchError ref
+      = formatError ("did not find unique match for " <> ref)
+
+    j :: (Show a) => (H.TaskRef -> TaskApplication a) -> H.Task -> TaskConfig a
+    j f v
+      = runTask (f $ H.taskRef v)
+
+    coerceToSuccess :: (Show a) => a -> Document
+    coerceToSuccess = formatSuccess . Text.pack . show
