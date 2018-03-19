51ab37c968b96c53343fcd60a070220ac4ed7375
Jack Palfrey <jack.palf@gmail.com>
2018-03-19 09:00:04 +0000

    removing unused instance

--- packages/event-clean/src/Main.hs
+++ packages/event-clean/src/Main.hs
@@ -7,52 +7,18 @@ module Main
   ( main
   ) where
 
-import Control.Monad.Trans.Resource
-import Data.Conduit as C
-import Data.List
-import Data.Maybe
-import Data.Semigroup ((<>))
+import qualified Control.Monad.Trans.Resource as R
+import qualified Data.Aeson                   as Aeson
+import qualified Data.ByteString              as S
+import qualified Data.ByteString.Lazy         as L
+import qualified Data.Conduit                 as C
+import qualified Data.Conduit.Combinators     as Cx
+import qualified Data.List                    as List
+import qualified Data.Maybe                   as M
+
 import Event
-import qualified Data.Aeson as Aeson
-import qualified Data.ByteString as S
-import qualified Data.ByteString.Lazy as L
-import qualified Data.Conduit.Combinators as Cx
-
-
-newtype K f m a = K (f -> (m a))
-
-
-instance EventBackend (K FilePath IO) where
-  readEvents = K kkk
-  writeEvent = K . flip jjj
-
-
-kkk :: (Aeson.FromJSON a) => FilePath -> IO [Event a]
-kkk file
-  = t <$> runResourceT
-      (  Cx.sourceFile file
-      $= splitLines
-      $$ Cx.sinkList
-      )
-
-  where
-    t :: (Aeson.FromJSON a) => [S.ByteString] -> [Event a]
-    t = catMaybes . fmap Aeson.decodeStrict
-
-
-jjj :: (Aeson.ToJSON a) => FilePath -> Event a -> IO ()
-jjj file xs
-  = runResourceT
-    $ C.yield (convert xs)
-    $$ Cx.sinkFile file
-
-  where
-    convert :: (Aeson.ToJSON a) => Event a -> S.ByteString
-    convert e = L.toStrict (Aeson.encode e) <> "\n"
-
-
-
-
+import Data.Semigroup ((<>))
+import Data.Conduit (($$), ($=))
 
 
 type SomeEvent = Event Aeson.Value
@@ -64,12 +30,12 @@ main
   >>= conduitWriteEvents file
 
   where
-    file = ".tjasks"
+    file = ".tasks"
 
 
 conduitReadEvents :: FilePath -> IO [Maybe SomeEvent]
 conduitReadEvents file = do
-  content <- runResourceT
+  content <- R.runResourceT
     $ Cx.sourceFile file
     $= splitLines
     $$ Cx.sinkList
@@ -79,7 +45,7 @@ conduitReadEvents file = do
 
 conduitWriteEvents :: FilePath -> [SomeEvent] -> IO ()
 conduitWriteEvents file xs
-  = runResourceT
+  = R.runResourceT
     $ Cx.yieldMany (convert <$> xs)
     $$ Cx.sinkFile file
 
@@ -88,9 +54,9 @@ conduitWriteEvents file xs
     convert e = L.toStrict (Aeson.encode e) <> "\n"
 
 
-splitLines :: Conduit S.ByteString (ResourceT IO) S.ByteString
+splitLines :: C.Conduit S.ByteString (R.ResourceT IO) S.ByteString
 splitLines = Cx.linesUnboundedAscii
 
 
 sortEvents :: [Maybe (Event a)] -> [Event a]
-sortEvents = sortOn timestamp . catMaybes
+sortEvents = List.sortOn timestamp . M.catMaybes
