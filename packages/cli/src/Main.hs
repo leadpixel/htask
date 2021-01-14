2a42969b496e0314f7fbe9cd18b56cfc52883a7f
Jack Palfrey <jack.palf@gmail.com>
2021-01-14 19:42:57 +0000

    removing orphan instance

--- packages/cli/src/Main.hs
+++ packages/cli/src/Main.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
 
 module Main
   ( main
@@ -12,31 +13,34 @@ import qualified HTask.CLI.Options          as Opt
 import qualified HTask.CLI.Render           as Render
 import qualified HTask.CLI.Runners          as Runner
 
-import Data.Time
-import Data.UUID
+import           Control.Monad.IO.Unlift
 import           Control.Monad.Random.Class
 import           Event.Backend.File
+import           Events
 import           Leadpixel.Provider
 
 
-newtype Thing a = Thing (FileEventBackend IO a)
+newtype App m a = App { runApp :: FileEventBackend m a }
   deriving (Functor, Applicative, Monad)
 
--- instance Provider UTCTime Thing where
---   gen = Thing $ T.lift gen
+instance (Monad m, MonadUnliftIO m) => HasEventSource (App m) where
+  readEvents = App readEvents
 
--- instance Provider UUID Thing where
---   gen = Thing $ T.lift gen
+instance (Monad m, MonadUnliftIO m) => HasEventSink (App m) where
+  writeEvent = App . writeEvent
 
-instance MonadRandom Thing where
-  getRandom = Thing $ T.lift getRandom
-  getRandoms = Thing $ T.lift getRandoms
-  getRandomR = Thing . T.lift . getRandomR
-  getRandomRs = Thing . T.lift . getRandomRs
+instance (Provider k m) => Provider k (App m) where
+  gen = App $ T.lift gen
+
+instance (MonadRandom m) => MonadRandom (App m) where
+  getRandom = App $ T.lift getRandom
+  getRandoms = App $ T.lift getRandoms
+  getRandomR = App . T.lift . getRandomR
+  getRandomRs = App . T.lift . getRandomRs
 
 
 main :: IO ()
 main = do
   options <- Opt.getOptions
-  let op = Runner.runAction (Opt.action options)
-  runFileBackend (Opt.taskfile options) op >>= Render.renderResult
+  let app = Runner.runAction (Opt.action options)
+  runFileBackend (Opt.taskfile options) (runApp app) >>= Render.renderResult
